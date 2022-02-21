21358453fdc743268f158d171175a2305e210751
Kaushik-08 <93571059+Kaushik-08@users.noreply.github.com>
2022-02-21 13:24:05 +0530

    Delete Javascript ex directory

--- Javascript ex/js/funct.js
+++
@@ -1,7 +0,0 @@
-let a = prompt("please enter first number","");
-let b = prompt("please enter second number","");
-
-var c = parseInt(a) + parseInt(b)
-  document.write("hello");
-  document.write("<br>")
-document.write("The sum of "+a+" and "+b+" is: "+c);
--- Javascript ex/js/sqroot.js
+++
@@ -1,6 +0,0 @@
-
-var a = prompt('please enter a number','')
-var b = Math.sqrt(a);
-document.write("the square root is");
-document.write("<br>");
-document.write(b);

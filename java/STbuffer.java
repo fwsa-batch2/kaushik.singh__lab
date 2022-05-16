969fa7843b9052da4589e1f7cdadedc84e4c66e3
Kaushik Singh <ckaushicksingh2003@gmail.com>
2022-05-16 17:50:32 +0530

    string buffers

---
+++ java/STbuffer.java
@@ -0,0 +1,53 @@
+public class STbuffer {
+//     public static void main(String[] args) {
+//     StringBuffer buf = new StringBuffer("kauhsik");
+//         buf.append(" singh");
+//         System.out.println(buf);
+// }
+
+
+
+// public static void main(String[] args) {
+    
+// StringBuffer buf = new StringBuffer("fresh");
+//         buf.insert(2, "works");
+//         System.out.println(buf);
+// }
+
+
+// public static void main(String[] args) {
+    
+//         StringBuffer buf = new StringBuffer("kaus");
+//         buf.replace(0, 2, "hik");
+//         System.out.println(buf);
+// }
+
+
+// public static void main(String[] args) {
+
+//         StringBuffer buf = new StringBuffer("freshworks");
+//         buf.delete(1, 5);
+//         System.out.println(buf);
+// }
+
+
+// public static void main(String[] args) {
+    
+        // StringBuffer buf = new StringBuffer("aswf");
+        // buf.reverse();
+        // System.out.println(buf);
+// }
+
+
+public static void main(String[] args) {
+   
+        StringBuffer buf = new StringBuffer();
+        System.out.println(buf.capacity());
+        buf.append("kaushik fwsa student");
+        System.out.println(buf.capacity());
+}
+}
+
+
+
+

195ba973a627a6f5eefddba5cf898407837d82df
Kaushik Singh <ckaushicksingh2003@gmail.com>
2022-05-16 17:13:11 +0530

    stringmeth

---
+++ java/STmeth.java
@@ -0,0 +1,37 @@
+public class STmeth {
+
+    // concat() join the value 
+
+// public static void main(String[] args) {
+//     String join = "Kaushik";
+//     System.out.println(join.concat(" singh"));
+// }
+
+
+    // split() split the value 
+
+// public static void main(String[] args) {
+//         String spt = "hello world";
+//         String[] exp = spt.split(" ", 2);
+//         for (String gro : exp)
+//             System.out.println(gro);
+//     }
+
+
+    // length() it will find length
+   
+// public static void main(String[] args) {
+//     String len = "kaushik";
+//     System.out.println(len.length());
+// }
+
+
+    // substring() it will 
+
+   public static void main(String[] args) {
+       String sub = "kaushiksingh";
+       System.out.println(sub.substring(1,5));
+   } 
+}
+
+

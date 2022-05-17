90c19de49b865e5f603de2447f4fd7e4003e709a
Kaushik Singh <ckaushicksingh2003@gmail.com>
2022-05-17 14:38:03 +0530

    string builders

---
+++ java/STbuilders.java
@@ -0,0 +1,58 @@
+public class STbuilders {
+
+    // public static void main(String[] args) {
+    //     StringBuffer bild = new StringBuffer("kauhshik");
+    //     bild.append(" singh");
+    //     System.out.println(bild);
+    // }
+
+
+    // public static void main(String[] args) {
+    //     StringBuffer bild = new StringBuffer("kaushik");
+    //     bild.insert(2, "singh");
+    //     System.out.println(bild);
+    // }
+
+
+    // public static void main(String[] args) {
+        
+    //     StringBuffer bild = new StringBuffer("tyshworks");
+    //     bild.replace(0, 2, "fre");
+    //     System.out.println(bild);
+    // }
+
+
+    // public static void main(String[] args) {
+    //     StringBuffer bild = new StringBuffer("fewsa");
+    //     bild.delete(1, 2);
+    //     System.out.println(bild);
+ 
+    // }
+
+
+    // public static void main(String[] args) {
+        
+    //     StringBuffer bild = new StringBuffer("kihsuak");
+    //     bild.reverse();
+    //     System.out.println(bild);
+    // }
+
+
+    public static void main(String[] args) {
+
+        StringBuffer bild = new StringBuffer();
+        System.out.println(bild.capacity());
+        bild.append("kaushik singh is a fwsa student");
+        System.out.println(bild.capacity());
+
+    }
+}
+
+
+
+
+
+
+
+
+
